ca30180ea07280993962a164e347f98a871ac5db
Konstantin Savusia <savconstantine@gmail.com>
2022-08-29 23:15:39 +0600

    logosvg loader

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import LogoSVG from './assets/logo.svg?component'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <LogoSVG alt="Vite logo" class="logo" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
