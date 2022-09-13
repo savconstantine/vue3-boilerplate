04a97d0edcf546dab09f116ecbf00f57bf489e51
Konstantin Savusia <savconstantine@gmail.com>
2022-09-13 07:59:10 +0600

    add header and footer component

---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <header>
+    <div class="bg-white border-gray-200 px-4 lg:px-6 pt-2.5 dark:bg-gray-800 lg:pt-14">
+      <h1 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+        vue3 boilerplate
+      </h1>
+    </div>
+  </header>
+</template>
