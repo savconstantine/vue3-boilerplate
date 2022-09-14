1a5269678b69f1febf1abe9a9f0ae38c592830cc
Konstantin Savusia <savconstantine@gmail.com>
2022-09-14 08:57:13 +0600

    new header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,15 @@
 <template>
-  <header>
-    <div class="bg-white border-gray-200 px-4 lg:px-6 pt-2.5 dark:bg-gray-800 lg:pt-14">
-      <h1 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
-        vue3 boilerplate
-      </h1>
+  <header
+    class="bg-gray-100 dark:bg-gray-800 sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]"
+  >
+    <div class="flex items-center justify-between px-4 py-3 sm:p-0">
+      <div>
+        <h1
+          class="text-3xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white"
+        >
+          vue3 boilerplate
+        </h1>
+      </div>
     </div>
   </header>
 </template>
