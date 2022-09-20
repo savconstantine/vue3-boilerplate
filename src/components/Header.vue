b5a2060bc368d514c3fb5f2241367383160faff9
Konstantin Savusia <savconstantine@gmail.com>
2022-09-20 22:56:01 +0600

    set max-width to header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,52 +1,53 @@
 <template>
   <header
-    class="bg-gray-100 dark:bg-gray-800 sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]"
+    class="bg-gray-100 dark:bg-gray-800 sm:px-4 sm:py-3 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]"
   >
-    <div class="flex items-center justify-between px-4 py-3 sm:p-0">
-      <div>
-        <h1
-          class="text-3xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white"
-        >
-          vue3 boilerplate
-        </h1>
+    <div class="max-w-screen-lg mx-auto sm:flex sm:justify-between sm:items-center">
+      <div class="flex items-center justify-between px-4 py-3 sm:p-0">
+        <div>
+          <h1
+            class="text-3xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white"
+          >
+            vue3 boilerplate
+          </h1>
+        </div>
+        <div class="sm:hidden">
+          <button
+            @click="isOpen = !isOpen"
+            type="button"
+            class="block text-gray-500 hover:text-white focus:text-white focus:outline-none"
+          >
+            <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+              <path
+                v-if="isOpen"
+                fill-rule="evenodd"
+                d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+              />
+              <path
+                v-if="!isOpen"
+                fill-rule="evenodd"
+                d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
-      <div class="sm:hidden">
-        <button
-          @click="isOpen = !isOpen"
-          type="button"
-          class="block text-gray-500 hover:text-white focus:text-white focus:outline-none"
+      <nav
+        :class="isOpen ? 'block' : 'hidden'"
+        class="px-2 pt-2 pb-4 sm:flex sm:p-0"
+      >
+        <RouterLink
+          class="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2"
+          to="/"
+          >Home</RouterLink
         >
-          <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
-            <path
-              v-if="isOpen"
-              fill-rule="evenodd"
-              d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
-            />
-            <path
-              v-if="!isOpen"
-              fill-rule="evenodd"
-              d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-            />
-          </svg>
-        </button>
-      </div>
+        <RouterLink
+          class="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2"
+          to="/about"
+          >About</RouterLink
+        >
+      </nav>
     </div>
-    <nav
-      :class="isOpen ? 'block' : 'hidden'"
-      class="px-2 pt-2 pb-4 sm:flex sm:p-0"
-    >
-      
-      <RouterLink
-        class="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2"
-        to="/"
-        >Home</RouterLink
-      >
-      <RouterLink
-        class="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2"
-        to="/about"
-        >About</RouterLink
-      >
-    </nav>
   </header>
 </template>
 
