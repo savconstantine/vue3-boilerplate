9948ca510a83a6ff715145927a108176cba235b7
Konstantin Savusia <savconstantine@gmail.com>
2022-09-25 17:11:30 +0600

    darklight mode detect system darklight theme setup

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,34 @@ import { RouterView } from "vue-router";
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
 
-
-let isDark = ref(true);
-
-isDark.value = localStorage.getItem("darkMode") == "true";
-
-function changeDarkLightMode() {
+const changeDarkLightMode = () => {
   isDark.value = !isDark.value;
   localStorage.setItem("darkMode", isDark.value);
-}
+};
+
+const hasDarkModeSetted = () => {
+  if (localStorage.getItem("darkMode")) {
+    return localStorage.getItem("darkMode") == "true";
+  }
+
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: light)").matches
+  ) {
+    return false;
+  }
+
+  return true;
+};
+
+let isDark = ref(hasDarkModeSetted());
 </script>
 
 <template>
   <div :class="isDark ? 'dark' : ''">
-    <div class="bg-white dark:bg-gray-800 text-gray-800 dark:text-gray-100 min-h-screen flex flex-col justify-between">
+    <div
+      class="bg-white dark:bg-gray-800 text-gray-800 dark:text-gray-100 min-h-screen flex flex-col justify-between"
+    >
       <Header @toggleDarkMode="changeDarkLightMode" :isDark="isDark" />
       <main class="max-w-screen-lg mx-auto">
         <RouterView v-slot="{ Component }">
