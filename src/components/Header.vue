66c7267634adc8df0bd9f0e249c7d3a212322e23
Konstantin Savusia <savconstantine@gmail.com>
2022-09-25 16:44:25 +0600

    fix hamburger menu button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,7 +12,7 @@ const props = defineProps({
 
 const emits = defineEmits(["toggleDarkMode"]);
 
-let isOpen = false;
+let isOpenMenu = ref(false);
 </script>
 
 <template>
@@ -32,18 +32,18 @@ let isOpen = false;
         </div>
         <div class="sm:hidden">
           <button
-            @click="isOpen = !isOpen"
+            @click="isOpenMenu = !isOpenMenu"
             type="button"
-            class="block text-gray-500 hover:text-white focus:text-white focus:outline-none"
+            class="block text-gray-500 hover:text-gray-500 focus:text-gray-500 shadow-none fill-gray-900 dark:fill-white"
           >
             <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
               <path
-                v-if="isOpen"
+                v-if="isOpenMenu"
                 fill-rule="evenodd"
                 d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
               />
               <path
-                v-if="!isOpen"
+                v-if="!isOpenMenu"
                 fill-rule="evenodd"
                 d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
               />
@@ -52,16 +52,16 @@ let isOpen = false;
         </div>
       </div>
       <nav
-        :class="isOpen ? 'block' : 'hidden'"
+        :class="isOpenMenu ? 'block' : 'hidden'"
         class="px-2 pt-2 pb-4 sm:flex sm:p-0"
       >
         <RouterLink
-          class="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2"
+          class="block px-2 py-1 text-gray-900 dark:text-white font-semibold sm:mt-0 sm:ml-2"
           to="/"
           >Home</RouterLink
         >
         <RouterLink
-          class="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2"
+          class="block px-2 py-1 text-gray-900 dark:text-white font-semibold sm:mt-0 sm:ml-2"
           to="/about"
           >About</RouterLink
         >
